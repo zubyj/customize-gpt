7b068c5b32a2507d91a61afaeb513cdffdd01ed0
Zuby Javed <zubyjaved@gmail.com>
2023-07-12 11:25:03 -0700

    change the font to comic sans because why not

--- content.js
+++ content.js
@@ -1,5 +1,15 @@
-let helloWorld = document.createElement('div');
-helloWorld.innerHTML = 'Hello World!';
-helloWorld.style.color = 'red';
-helloWorld.style.fontSize = '48px';
-document.body.appendChild(helloWorld);
+// let helloWorld = document.createElement('div');
+// helloWorld.innerHTML = 'Hello World!';
+// helloWorld.style.color = 'red';
+// helloWorld.style.fontSize = '48px';
+// document.body.appendChild(helloWorld);
+
+// remove the dark class and dark style from documentElement
+document.documentElement.classList.remove('dark');
+
+
+
+document.documentElement.style.backgroundColor = 'gold';
+document.body.style.backgroundColor = 'gold';
+document.body.style.color = 'black';
+document.body.style.fontFamily = 'Comic Sans MS, Comic Sans, cursive';
