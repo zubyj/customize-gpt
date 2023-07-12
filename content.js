d6bafed46f7ccbcba089f55998344bf32a7985eb
Zuby Javed <zubyjaved@gmail.com>
2023-07-12 14:27:38 -0700

    move customize button to left side

--- content.js
+++ content.js
@@ -7,24 +7,27 @@ chrome.runtime.onMessage.addListener((request) => {
     if (request.message === 'tab_updated') {
 
         let customizeButton = document.createElement('button');
+        customizeButton.style.width = '28px';
+        customizeButton.style.height = '28px';
         customizeButton.style.textContent = 'Customize'
         customizeButton.style.backgroundImage = 'url(' + chrome.runtime.getURL('customize-icon.png') + ')';
         customizeButton.style.backgroundRepeat = 'no-repeat';
         customizeButton.style.backgroundSize = 'contain';
-        customizeButton.style.width = '25px';
-        customizeButton.style.height = '25px';
-        customizeButton.style.borderRadius = '5px';
-        customizeButton.style.margin = '0 auto';
-        customizeButton.style.marginLeft = '90%';
+        customizeButton.style.backgroundPosition = 'center';
+        customizeButton.style.margin = '0';
         customizeButton.onclick = function () {
             chrome.runtime.sendMessage({ message: 'open_new_tab', url: chrome.runtime.getURL('options.html') });
         };
 
+
         let textArea = document.getElementById('prompt-textarea');
-        textArea.parentElement.appendChild(customizeButton, textArea);
+        // insert customize button adjacent to text area
+        textArea.parentElement.insertAdjacentElement('afterbegin', customizeButton);
 
         chrome.storage.sync.get(['color', 'backgroundColor', 'fontFamily'], function (items) {
             document.body.style.color = items.color;
+            document.documentElement.style.color = items.color;
+            document.documentElement.style.backgroundColor = items.backgroundColor;
             document.body.style.backgroundColor = items.backgroundColor;
             document.body.style.fontFamily = items.fontFamily;
         });
