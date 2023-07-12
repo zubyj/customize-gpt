46667fe0d8a9a9daa16b2abd75eb7496d7325df8
Zuby Javed <zubyjaved@gmail.com>
2023-07-12 14:01:18 -0700

    let the user select the background color using a color wheel/picker

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && tab.url.includes('chat.openai.com')) {
+        chrome.tabs.sendMessage(tabId, { message: 'tab_updated' });
+    }
+});
+
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.message === 'open_new_tab') {
--- content.js
+++ content.js
@@ -1,30 +1,31 @@
+
+
 document.documentElement.classList.remove('dark');
 
-let customizeButton = document.createElement('button');
-customizeButton.style.backgroundImage = 'url(' + chrome.runtime.getURL('customize-icon.png') + ')';
-customizeButton.style.backgroundRepeat = 'no-repeat';
-customizeButton.style.backgroundSize = 'contain';
-customizeButton.style.backgroundPosition = 'center';
-customizeButton.style.width = '48px';
-customizeButton.style.height = '48px';
-customizeButton.style.fontSize = '24px';
-customizeButton.style.border = '1px solid purple';
-customizeButton.onclick = function () {
-    chrome.runtime.sendMessage({ message: 'open_new_tab', url: chrome.runtime.getURL('options.html') });
-};
+// When the tab is finished loading, add the mic button to the page
+chrome.runtime.onMessage.addListener((request) => {
+    if (request.message === 'tab_updated') {
 
-let div = document.getElementById('prompt-textarea').parentElement;
+        let customizeButton = document.createElement('button');
+        customizeButton.style.textContent = 'Customize'
+        customizeButton.style.backgroundImage = 'url(' + chrome.runtime.getURL('customize-icon.png') + ')';
+        customizeButton.style.backgroundRepeat = 'no-repeat';
+        customizeButton.style.backgroundSize = 'contain';
+        customizeButton.style.backgroundPosition = 'center';
+        customizeButton.style.width = '30x';
+        customizeButton.style.height = '30px';
+        customizeButton.style.border = '1px solid purple';
+        customizeButton.onclick = function () {
+            chrome.runtime.sendMessage({ message: 'open_new_tab', url: chrome.runtime.getURL('options.html') });
+        };
 
-let wrapperDiv = document.createElement('div');
-wrapperDiv.style.display = 'flex';
-wrapperDiv.style.justifyContent = 'center';
-wrapperDiv.style.alignItems = 'center';
-div.appendChild(wrapperDiv);
+        let textArea = document.getElementById('prompt-textarea');
+        textArea.parentElement.insertBefore(customizeButton, textArea);
 
-div.parentElement.appendChild(customizeButton);
-
-chrome.storage.sync.get(['color', 'backgroundColor', 'fontFamily'], function (items) {
-    document.body.style.color = items.color;
-    document.body.style.backgroundColor = items.backgroundColor;
-    document.body.style.fontFamily = items.fontFamily;
+        chrome.storage.sync.get(['color', 'backgroundColor', 'fontFamily'], function (items) {
+            document.body.style.color = items.color;
+            document.body.style.backgroundColor = items.backgroundColor;
+            document.body.style.fontFamily = items.fontFamily;
+        });
+    }
 });
